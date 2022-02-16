e9bb1b4860d18916eea5449185011967dab92123
Samantha Le Huec <s.lhuec@gmail.com>
2022-02-17 00:05:07 +0100

    partiellement correct, buffer/pb mem a debugg

--- gnl_brouillon.c
+++ gnl_brouillon.c
@@ -54,88 +54,6 @@ int ft_strchr(char *s, int c)
 		return (-1);
 }
 
-/*
-char	*get_next_line(int fd, char *c)
-{
-static int i;
-int j;
-char *line;
-char *rd_buf;
-
-rd_buf= malloc(sizeof(char) * (2));
-
-j = i;
-if (rd_buf[j] == '\0' && rd_buf[i] == '\0')
-{
-	return (NULL);
-}
- while (rd_buf[j] != '\n' && rd_buf[j])
-{
-	my_read(c, rd_buf, 2); 
-	
-	j++;
-}
-
-	my_read(c, rd_buf, 2); 
-
- while (j < 2)
-{
-	if (rd_buf[j] == '\n')
-	{
-		rd_buf[j] = '\0';
-		return (rd_buf);
-	}
-	j++;
-}
-malloc(sizeof(char) * (rd_buf + 2));
-
-
-line = ft_substr(rd_buf, i, j - i);
-i = j;
-if (rd_buf[j] == '\n')
-	i++;
-return (line);
-}
-
-char *get_one_line(char *c)
-{
-	char *result;
-	char *tmp;
-	unsigned int block_size;
-	unsigned int i;
-	int ret;
-	int finish;
-
-	i = 1;
-	block_size = 3;
-	finish = 0;
-	result = NULL;
-
-	while (finish == 0)
-	{
-		tmp = result;
-		result = malloc(sizeof(char) * (block_size * i) + 1);
-		if (!result)
-			return (NULL);
-
-		if (tmp != NULL)
-			ft_strcpy(result, tmp);
-		ret = my_read(c + block_size * (i - 1), result + block_size * (i - 1), block_size);
-		result[ret + block_size * (i - 1)] = '\0';
-
-		if (ft_strchr(result, '\n') != -1)
-		{
-			finish = 1;
-			ret = ft_strchr(result, '\n');
-			result[ret] = '\0';
-		}
-
-		i++;
-	}
-	return (result);
-}
-*/
-
 size_t  ft_strlen(char *str)
 {
 		size_t  i;
@@ -148,59 +66,51 @@ size_t  ft_strlen(char *str)
 		return (i);
 }
 
-
-char *get_one_line_fd(int fd)
+char *get_next_line(int fd)
 {
 	char			*line;
 	char			*tmp;
 	static char		*save;
 	unsigned int	i;
-	int				ind;
+	int				len;
 	int				ret;
 	int				finish;
 
-	i = 1;
+	i = 1 + !!save;
 	finish = 0;
-	// /!\ gerer le cas ou il y a deja des \n dans save/!\
-	// ca sert a rien de lire s il y  a deja le \n a traiter
-	//strcpy de save? a chaque appel de gnl
 	line = save;
-
+	save = 0;
+	
 	while (finish == 0)
 	{
-		tmp = line;
-			// a chaque tour de boucle a l endroit ou 
-			// j'assigne line a tmp je cree un leak 
-	
-		line = malloc(sizeof(char) * (BUFFER_SIZE * i) + sizeof(char));
-		//au lieu de +1 pour le \0
-		if (!line)
-			return (NULL);
-		if (tmp != NULL)
-			ft_strcpy(line, tmp);
-		// avant de read je dois traiter le contenu de save transfere
-		//dans line
-		ret = read(fd, line + BUFFER_SIZE * (i - 1), BUFFER_SIZE);
-		line[ret + BUFFER_SIZE * (i - 1)] = '\0';
-		if (ret == 0)
-			return (NULL); 
-		if (ft_strchr(line, '\n') != -1)
+		if (line != NULL && ft_strchr(line, '\n') != -1)
 		{
 			finish = 1;
 			ret = ft_strchr(line, '\n');
 			if (line[ret + 1]) 
 			{
-				ind = line[ret + 1];
-				while (line[ind])
-				{
-					ft_strlen(&line[ind]);
-					ind ++;
-				}
-				save = ft_substr(line, ret + 1, ind);
-			}		
-		
+				len = ft_strlen(&line[ret + 1]);
+				save = ft_substr(line, ret + 1, len);
+			}			
 			line[ret + 1] = '\0';
-	}
+			return (line);
+		}
+		tmp = line;
+		len = 0;
+		line = malloc(sizeof(char) * (BUFFER_SIZE * i) + sizeof(char));
+		if (!line)
+			return (NULL);
+		if (tmp != NULL)
+		{
+			ft_strcpy(line, tmp);
+			len = ft_strlen(line);
+		}
+		ret = read(fd, line + len, BUFFER_SIZE);
+		line[ret + len] = '\0';
+		if (ret == 0 && ft_strlen(line) == 0)
+			return (NULL);
+		else if (ret == 0)
+			return (line);	
 		i++;
 	}
 	return (line);
@@ -211,11 +121,12 @@ int main()
 	int fd;
 	char *line;
 	fd = open("fichier.txt", O_RDWR);
-		while (1) {
-		line = get_one_line_fd(fd);
-		printf("%s\n", line);
+	while (1) 
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
 		free(line);
-	if (line == NULL)
-		break;
+			if (line == NULL)
+				break;
 	}
 }
