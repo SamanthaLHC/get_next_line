f8b0b8b6650d2a202c45f15f7a4b7a7df9da907b
Samantha LE HUEC <sle-huec@e2r4p2.clusters.42paris.fr>
2022-02-17 19:09:47 +0100

    a segmenter

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 15:49:48 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/02/06 15:51:38 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/02/17 18:52:59 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,41 +24,44 @@ char *get_next_line(int fd)
 	unsigned int	i;
 	int				len;
 	int				ret;
-	int				finish;
+	//int				finish;
 
 	i = 1 + !!save;
-	finish = 0;
+	//finish = 0;
 	line = save;
 	save = NULL;
+	len = 0;
 
-	while (finish == 0)
+	//while (finish == 0)
+	while (1)
 	{
 		if (line != NULL && ft_strchr(line, '\n') != -1)
 		{
-			finish = 1;
+			//finish = 1;
 			ret = ft_strchr(line, '\n');
-			if (line[ret + 1]) 
-			{
-				len = ft_strlen(&line[ret + 1]);
-				save = ft_substr(line, ret + 1, len);
-			}			
+			//len = ft_strlen(&line[ret + 1]);
+			save = ft_substr(line, ret + 1, ft_strlen(&line[ret + 1]));
 			line[ret + 1] = '\0';
 			return (line);
 		}
+		//line = retour de fonction ligne 36 a 43;
 		tmp = line;
-		len = 0;
-		line = malloc(sizeof(char) * (BUFFER_SIZE * i) + sizeof(char));
+		//len = 0;
+		line = malloc(sizeof(char) * (BUFFER_SIZE * i + 1));
 		if (!line)
 			return (NULL);
+		//transformer la condition du dessous en len = ft_check(line, tmp);
 		if (tmp)
 		{
 			ft_strcpy(line, tmp);
-				free(tmp);
+			free(tmp);
 			len = ft_strlen(line);
 		}
+		//proteger le read si jamais return -1
 		ret = read(fd, line + len, BUFFER_SIZE);
 		line[ret + len] = '\0';
-		if (ret == 0 && ft_strlen(line) == 0)
+		if (ret == 0 && line[0] == '\0')
+		//if (ret == 0 && ft_strlen(line) == 0)
 		{
 			free(save);
 			free(line);
