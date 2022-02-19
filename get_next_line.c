a5ec52f516fa8d6a2946cc6cdb7611a97538a2aa
Samantha Le Huec <s.lhuec@gmail.com>
2022-02-19 14:37:37 +0100

    FINISH??

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 15:49:48 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/02/17 18:52:59 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/02/19 14:35:26 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,63 +16,75 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+int	get_save(char **save ,char *line)
+{
+	int		ret;
+	if (line != NULL && ft_strchr(line, '\n') != -1)
+	{
+		ret = ft_strchr(line, '\n');
+		*save = ft_substr(line, ret + 1, ft_strlen(&line[ret + 1]));
+		line[ret + 1] = '\0';
+		return (1);
+	}
+	return (0);
+}
+
+int	increase_buff(char **line, unsigned i)
+{
+	char			*tmp;
+	int				len;
+
+	len = 0;
+	tmp = *line;
+	*line = malloc(sizeof(char) * (BUFFER_SIZE * i + 1));
+	if (!*line)
+		return (-1);
+	if (tmp)
+	{
+		ft_strcpy(*line, tmp);
+		free(tmp);		
+		len = ft_strlen(*line);
+	}
+	return (len);
+}
+
+char *last_read(char *line)
+{
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);	
+}
+
+char	*get_next_line(int fd)
 {
 	char			*line;
-	char			*tmp;
 	static char		*save;
 	unsigned int	i;
 	int				len;
 	int				ret;
-	//int				finish;
 
 	i = 1 + !!save;
-	//finish = 0;
 	line = save;
 	save = NULL;
-	len = 0;
-
-	//while (finish == 0)
 	while (1)
 	{
-		if (line != NULL && ft_strchr(line, '\n') != -1)
-		{
-			//finish = 1;
-			ret = ft_strchr(line, '\n');
-			//len = ft_strlen(&line[ret + 1]);
-			save = ft_substr(line, ret + 1, ft_strlen(&line[ret + 1]));
-			line[ret + 1] = '\0';
-			return (line);
-		}
-		//line = retour de fonction ligne 36 a 43;
-		tmp = line;
-		//len = 0;
-		line = malloc(sizeof(char) * (BUFFER_SIZE * i + 1));
-		if (!line)
+		if (get_save(&save, line))
+			return (line);		
+		if ((len = increase_buff(&line, i)) == -1)
 			return (NULL);
-		//transformer la condition du dessous en len = ft_check(line, tmp);
-		if (tmp)
-		{
-			ft_strcpy(line, tmp);
-			free(tmp);
-			len = ft_strlen(line);
-		}
-		//proteger le read si jamais return -1
-		ret = read(fd, line + len, BUFFER_SIZE);
+		if ((ret = read(fd, line + len, BUFFER_SIZE)) == -1)
+			return (NULL);
 		line[ret + len] = '\0';
-		if (ret == 0 && line[0] == '\0')
-		//if (ret == 0 && ft_strlen(line) == 0)
-		{
-			free(save);
-			free(line);
-			return (NULL);
-		}
-		else if (ret == 0)
-			return (line);	
+		if (ret == 0)
+			return last_read(line);
 		i++;
 	}
 	return (line);
 }
+
 
 #include <stdio.h>
 #include <fcntl.h>
